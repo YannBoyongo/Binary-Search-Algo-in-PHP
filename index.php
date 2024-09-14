5abcf1cca8896b431a9a6ed383c89ff8c76ba2a7
Yann Boyongo <yaboyon3@gmail.com>
2024-09-14 12:00:06 +0200

    wrinting a function to search a key and determine it value in the array using binary search

--- index.php
+++ index.php
@@ -1,2 +1,28 @@
 <?php
 
+function searchKey($arr, $key){  
+    $left = 0;
+    $right = count($arr) - 1;
+
+    while($left <= $right){
+        $middle = floor(($left + $right) / 2);
+
+        if($arr[$middle] == $key){
+            return $middle;
+        }
+
+        if($key < $arr[$middle]){
+            $right = $middle - 1;
+        }else{
+            $left = $middle + 1;
+        }
+    }
+    
+    return false;
+}
+
+
+$arr = [-5, -1, 2, 6, 9, 11, 15, 22, 27, 34];
+$key = 9;
+
+echo searchKey($arr, $key);
